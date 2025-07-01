2a08b2aeedf1caed9bb28e64b6373e59e460949f
Zhmerik <besterik55@gmail.com>
2025-07-01 20:04:18 +0300

    Add beting system (partly)

--- obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19591e7cc370d35e24cf367f1443f40793446929")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b365daad11bfa1d88fa0e9a2d68632137fdf7350")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
